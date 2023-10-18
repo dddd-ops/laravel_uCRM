b601274806c18ea7323618289211fcec2ea58cd6
ff <dd.12345.ev@gmail.com>
2023-10-18 13:20:51 +0900

    temp

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\AnalysisController;
 
+Route::get('analysis', [AnalysisController::class, 'index'])->name('analysis');
 
 Route::resource('items', ItemController::class)
     ->middleware(['auth', 'verified']);
